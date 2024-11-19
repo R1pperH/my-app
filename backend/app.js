16d2f4804315d89a7846ff90ca69db072453e67f
R1peerH <harshfn0207@gmail.com>
2024-11-20 00:50:48 +0530

    ye nhi pata

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
