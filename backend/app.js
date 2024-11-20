fa03c759fee89d5f7e342640fda18926a5c4d956
R1pperH <harshfn0207@gmail.com>
2024-11-21 01:37:36 +0530

    baj gaya band

--- backend/app.js
+++ backend/app.js
@@ -7,3 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+mongoose
+  .connect(
+    `mongodb+srv://harshfn0207:aVXO0vct2CMjpoSn@shoplu.kk5ff.mongodb.net/`
+  )
+  .then(() => console.log("Connected to database"));
+app.listen(5000);
