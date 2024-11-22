a3b1cfa66b39d44a06be3021a52eb4c244aa0807
R1pperH <harshfn0207@gmail.com>
2024-11-23 03:31:11 +0530

    ek database ka function

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -46,7 +46,45 @@ const getProductById = (req, res, next) => {
   res.json(product);
 };
 
+const addToCart = async (req, res, next) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  const { userId } = req.body;
+  const user = await User.findById(userId);
+
+  if (!product) {
+    console.log("no product found");
+  }
+
+  if (!user.isAdmin) {
+    return;
+  }
+
+  const productAtIndex = user.cart.findIndex((item) =>
+    item.product.equals(product._id)
+  );
+  if (productAtIndex > -1) {
+    user.cart[productAtIndex].quantity += 1;
+  } else {
+    user.cart.push({ product: product._id, quantity: 1 });
+  }
+
+  await user.save();
+  await user.populate("cart.product");
+  res.json("added to cart");
+};
+
+const removeFromCart = (req, res, next) => {
+  const { id } = req.params;
+  const { userId } = req.body;
+
+  const user = User.findById(userId);
+  const prodinCart = user.cart.filter((product) => product._id !== id);
+  prodinCart.save();
+};
+
 router.get("/", getProducts);
 router.post("/addProduct/:id", addProducts);
+router.post("/addtoCart/:id", addToCart);
 
 module.exports = router;
